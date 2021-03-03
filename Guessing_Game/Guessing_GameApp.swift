726490a2febd6b6157a86f7ba933cda87fb9301b
Jim <chenjim821ddt@gmail.com>
2021-03-03 23:18:39 +0800

    Initial Commit

---
+++ Guessing_Game/Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_GameApp.swift
+//  Guessing_Game
+//
+//  Created by 陳元浚 on 2021/3/3.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
